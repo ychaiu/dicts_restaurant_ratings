916e7e74194537741eaab119adfb93ce7bce6085
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-29 12:41:41 -0700

    Add user input

--- ratings.py
+++ ratings.py
@@ -3,23 +3,40 @@
 def ratings_dict(filename):
 
 	restaurant_ratings = open(filename,"r")
-	contents = restaurant_ratings.read()
-	lined_contents = contents.splitlines()
 
 	ratings_dict = {}
 
-	for line in lined_contents:
-		name_rating = line.split(":")
-		name = name_rating[0]
-		rating = name_rating[1]
+	for line in restaurant_ratings:
+		name, rating = line.rstrip().split(":")
 	
 		ratings_dict[name] = rating
 	
 	sorted_keys = sorted(ratings_dict.keys())
-	for key in sorted_keys:
-	 	print("{} is rated at {}.".format(key, ratings_dict[key]))
+	# for key in sorted_keys:
+	#  	print("{} is rated at {}.".format(key, ratings_dict[key]))
 
-		
-ratings_dict("scores.txt")
+	return ratings_dict
 
 # put your code here
+
+def user_ratings():
+	user_dict = ratings_dict("scores.txt")
+	name = input("What is the name of the restaurant?")
+	score = input ("What is your rating for this restaurant?")
+	user_dict[name] = score
+	
+	sorted_keys = sorted(user_dict.keys())
+	for key in sorted_keys:
+	 	print("{} is rated at {}.".format(key, user_dict[key]))
+
+
+user_ratings()
+
+
+#  Traceback (most recent call last):
+#   File "ratings.py", line 14, in <module>
+#     ratings_dict("scores.txt")
+#   File "ratings.py", line 10, in ratings_dict
+#     restaurant_name =restaurant_ratings.split(":")[0]
+# AttributeError: '_io.TextIOWrapper' object has no attribute 'split'
+
